8f239c9095922917d98c47868588489f54c7d4ca
Andrew Pritchard <andrewpritchard@users.noreply.github.com>
2019-07-12 05:47:40 +1000

    Fixed the broken task options & gave the functions display names so they no longer appear as anonymous in node.js

--- gulpfile.js
+++ gulpfile.js
@@ -12,54 +12,69 @@ let sassTasks = require('./bass/sass.json');
 
 //https://stackoverflow.com/questions/29885220/using-objects-in-for-of-loops/#29885220
 for (const key in sassTasks) {
+    let config = sassTasks[key];
+
     sassTasks[key] = function() {
-        return gulp.src('./default.scss')
+        return gulp.src(config.src)
             .pipe(plugins.sass({
                 // [https://github.com/sass/node-sass#outputstyle]
-                outputStyle: '\' + sassTasks[key].options.outputStyle + \'',
-                precision: '\' + sassTasks[key].options.precision + \'',
-                sourceComments: '\' + sassTasks[key].options.sourceComments + \''
+                outputStyle: config.options.outputStyle,
+                precision: config.options.precision,
+                sourceComments: config.options.sourceComments
             }).on('error', plugins.sass.logError))
-            .pipe(gulp.dest('./css'));
+            .pipe(gulp.dest(config.dest));
+
+        cb();
     };
+
+    sassTasks[key].displayName = key;
 };
 
 let autoprefixerTasks = require('./bass/autoprefixer.json');
 
-//https://stackoverflow.com/questions/29885220/using-objects-in-for-of-loops/#29885220
 for (const key in autoprefixerTasks) {
+    let config = autoprefixerTasks[key];
+
     autoprefixerTasks[key] = function() {
-        return gulp.src('./css/default.css')
+        return gulp.src(config.src)
             .pipe(plugins.autoprefixer({
                 // [https://github.com/postcss/autoprefixer#options]
-                env: '\' + autoprefixerTasks[key].options.env + \'',
-                cascade: '\' + autoprefixerTasks[key].options.cascade + \'',
-                remove: '\' + autoprefixerTasks[key].options.remove + \'',
-                supports: '\' + autoprefixerTasks[key].options.supports + \'',
-                flexbox: '\' + autoprefixerTasks[key].options.flexbox + \'',
-                grid: '\' + autoprefixerTasks[key].options.grid + \'',
-                //stats: '\' + autoprefixerTasks[key].options.stats + \'',
-                //browsers: '\' + autoprefixerTasks[key].options.browsers + \'',
-                ignoreUnknownVersions: '\' + autoprefixerTasks[key].options.ignoreUnknownVersions + \''
+                env: config.options.env,
+                cascade: config.options.cascade,
+                remove: config.options.remove,
+                supports: config.options.supports,
+                flexbox: config.options.flexbox,
+                grid: config.options.grid,
+                overrideBrowserslist: config.options.overrideBrowserslist,
+                ignoreUnknownVersions: config.options.ignoreUnknownVersions
             }))
-            .pipe(gulp.dest('./css'));
+            .pipe(gulp.dest(config.dest));
+
+        cb();
     };
+
+    autoprefixerTasks[key].displayName = key;
 };
 
 let cssoTasks = require('./bass/csso.json');
 
-//https://stackoverflow.com/questions/29885220/using-objects-in-for-of-loops/#29885220
 for (const key in cssoTasks) {
+    let config = cssoTasks[key];
+
     cssoTasks[key] = function() {
-        return gulp.src('./css/default.css')
+        return gulp.src(config.src)
             .pipe(plugins.csso({
                 // [https://github.com/css/csso#usage-data]
-                restructure: '\' + cssoTasks[key].options.restructure + \'',
-                debug: '\' + cssoTasks[key].options.debug + \'',
-                usage: '\' + cssoTasks[key].options.usage + \''
+                restructure: config.options.restructure,
+                debug: config.options.debug,
+                usage: config.options.usage
             }))
-            .pipe(gulp.dest('./css'));
+            .pipe(gulp.dest(config.dest));
+
+        cb();
     };
+
+    cssoTasks[key].displayName = key;
 };
 
 exports.default = series(sassTasks.sass_default, autoprefixerTasks.autoprefixer_default, cssoTasks.csso_default);
