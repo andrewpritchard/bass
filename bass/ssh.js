7eb1f70a2e6c8bc2d81fa2b1b293e7fee0421ab7
Andrew Pritchard <andrewpritchard@users.noreply.github.com>
2019-09-26 07:57:28 +1000

    Added a new helper function to the SSH tasks to help reference the home directory correctly for key files & allows a JSON file to be used to allow multiple user credentials for a project

--- bass/ssh.js
+++ bass/ssh.js
@@ -7,11 +7,30 @@
 // Pattern for getting arguments from the gulpfile into an external file [https://stackoverflow.com/questions/13151693/passing-arguments-to-require-when-loading-module/#13163075]
 module.exports = function(gulp, plugins) {
     // Brings in the ability to read key files
-    const fs = require('fs');
-    // Helper to determine if a key file is ready to be read & used
-    function read_file(path) {
-        if(path != null) {
-            return fs.readFileSync(path);
+    plugins.fs = require('fs');
+    // Prints out the home directory
+    plugins.homeDir = require('os').homedir();
+    // Helper which tests for different key configurations & outputs the result
+    function key_test(path, home, key) {
+        // Recommended to use 'let' instead of 'var' to avoid scope issues [https://medium.com/podiihq/javascript-variables-should-you-use-let-var-or-const-394f7645c88f#ec5a]
+        let construct_path = path;
+        // Stops errors if the value returns falsy [https://developer.mozilla.org/en-US/docs/Glossary/Falsy]
+        if(path === false) {
+            // Checks for JSON
+            const is_json = path.includes('.json');
+            // Is the location under home?
+            if(home && path === false) {
+                construct_path = plugins.homeDir + path;
+                if(is_json) {
+                    // Loads the JSON file & retrieves the value associated with the key
+                    construct_path = plugins.homeDir + require(construct_path)[key];
+                };
+            } else if(is_json) {
+                // Relative reference means the 'bass' folder needs to be removed to get to the gulpfile root
+                construct_path = '.' + require(__dirname.replace('/bass', '') + construct_path)[key];
+            };
+            // Reads the key file
+            return plugins.fs.readFileSync(construct_path);
         } else {
             return path;
         };
@@ -28,9 +47,9 @@ module.exports = function(gulp, plugins) {
                 // Client options [https://github.com/mscdex/ssh2#client-methods]
                 host: config.connection.host,
                 port: config.connection.port,
-                username: config.connection.username,
+                username: key_test(config.connection.username, config.connection.useHomeDirectory, 'username'),
                 password: config.connection.password,
-                privateKey: read_file(config.connection.privateKey),
+                privateKey: key_test(config.connection.privateKey, config.connection.useHomeDirectory, 'privateKey'),
                 useAgent: config.connection.useAgent,
                 passphrase: config.connection.passphrase
             },
